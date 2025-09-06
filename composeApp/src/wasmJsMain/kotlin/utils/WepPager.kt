2f1cd53d863366b85e2a72abbac2aacb64b41ea3
ashokkanigiri <ashok.k@keka.com>
2025-09-06 21:49:04 +0530

    optimisations

--- composeApp/src/wasmJsMain/kotlin/utils/WepPager.kt
+++ composeApp/src/wasmJsMain/kotlin/utils/WepPager.kt
@@ -3,6 +3,8 @@ package utils
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -12,10 +14,15 @@ fun WebPager(
 ) {
     val pagerState = rememberPagerState(pageCount = { pages.size })
 
-    VerticalPager(
-        state = pagerState,
-        modifier = modifier
-    ) { page ->
-        pages.getOrNull(page)?.invoke()
+    CompositionLocalProvider {
+        VerticalPager(
+            state = pagerState,
+            modifier = modifier
+        ) { page ->
+            // Keep each composable stable with remember
+            key(page) {
+                pages[page].invoke()
+            }
+        }
     }
 }

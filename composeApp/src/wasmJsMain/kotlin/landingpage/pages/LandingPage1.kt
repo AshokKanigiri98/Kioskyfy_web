a31565dbc31f62da8f0932831437bb488f4c2687
ashokkanigiri <ashok.k@keka.com>
2025-09-06 09:56:14 +0530

    added icon

--- composeApp/src/wasmJsMain/kotlin/landingpage/pages/LandingPage1.kt
+++ composeApp/src/wasmJsMain/kotlin/landingpage/pages/LandingPage1.kt
@@ -2,20 +2,53 @@ package landingpage.pages
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import compose_example.composeapp.generated.resources.Res
+import compose_example.composeapp.generated.resources.logo_black
 import org.jetbrains.compose.resources.painterResource
-import utils.addBorder
 
 @Composable
 internal fun LandingPage1() {
-    Column {
+    Column(modifier = Modifier.background(color = Color.Black).padding(30.dp)) {
+        HeaderComponent()
+        BodyComponent()
+
+    }
+}
+
+@Composable
+private fun HeaderComponent() {
+    Image(
+        painterResource(Res.drawable.logo_black),
+        contentDescription = "Kotlin Logo",
+        modifier = Modifier.size(height = 450.dp, width = 800.dp)
+    )
+}
+
+@Composable
+private fun BodyComponent() {
+    Surface(
+        shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp), // only top rounded
+        elevation = 15.dp,
+        color = Color.Gray
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp) // inner padding
+        ) {
+            // Your content here
+        }
     }
 }

a0e11f646d2725f02077b4afcb71e25cc60da6ed
ashokkanigiri <ashok.k@keka.com>
2025-09-06 10:25:43 +0530

    mid

--- composeApp/src/androidMain/kotlin/org/example/project/LandingScreen1.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/LandingScreen1.kt
@@ -1,14 +1,12 @@
-package landingpage.pages
+package org.example.project
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
